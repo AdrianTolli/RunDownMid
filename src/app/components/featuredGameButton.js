5e0e13dd79df0bc62bb71ea6507460323c33d1b5
Adrian Tollisen <adrian.tollisen@hotmail.com>
2017-06-21 22:54:54 +0200

    Fixed featured button

--- src/app/components/featuredGameButton.js
+++ src/app/components/featuredGameButton.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import fetch from 'isomorphic-fetch';
 import { connect } from 'react-redux';
 import { setName } from '../redux/action';
 import { withRouter } from 'react-router-dom';
 import './featuredGameButton.css';
 
-class featuredGameButton extends React.Component {
+class FeaturedGameButton extends React.Component {
     constructor(){
         super();
         this.buttonClicked = this.buttonClicked.bind(this);
     };
 
+
+
     buttonClicked(){
         console.log('Featured game button clicked!');
+        fetch('/api/featuredGame/euw1/')
+            .then((response) => {
+                return response.json();
+            }).then((json) => {
+                let summoner = json.gameList[0].participants[0].summonerName;
+                this.props.history.push('/Gamepage/' + summoner );
+            });
+
         this.props.history.push('/Gamepage/'  );
     };
 
@@ -21,4 +32,4 @@ class featuredGameButton extends React.Component {
 
 };
 
-export default withRouter(featuredGameButton);
+export default withRouter(FeaturedGameButton);
--- src/app/views/Frontpage.js
+++ src/app/views/Frontpage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
-import featuredGameButton from '../components/featuredGameButton';
+import FeaturedGameButton from '../components/featuredGameButton';
 import './Frontpage.css';
 
 
@@ -9,7 +9,7 @@ export default class Frontpage extends React.Component {
         return <div className='frontpage'>
                 <h1> Run down mid</h1>
                 <SearchBar/>
-                <featuredGameButton/>
+                <FeaturedGameButton/>
             </div>
     }
 }
